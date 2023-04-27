7a52b1b34d621cc05c71c5857247731898d57dcc
Irving Caamal <irvv.17@gmail.com>
2023-04-27 09:35:45 -0500

    feat(upgrade class based components): to functional components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,50 +1,54 @@
-import * as React from 'react';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 import { Provider as StyletronProvider } from 'styletron-react';
-import { Server, Sheet } from 'styletron-engine-atomic';
+import { Server, Sheet } from 'styletron-engine-atomic/index';
 import { styletron } from '../helpers/styletron';
 
-class MyDocument extends Document<{ stylesheets: Sheet[] }> {
-  static async getInitialProps(context) {
-    const renderPage = () =>
-      context.renderPage({
-        // eslint-disable-next-line react/display-name
-        enhanceApp: (App) => (props) => (
+function MyDocument({ stylesheets }: { stylesheets: Sheet[] }) {
+  return (
+    <Html>
+      <Head>
+        {stylesheets.map((sheet, i) => (
+          <style
+            className="_styletron_hydrate_"
+            dangerouslySetInnerHTML={{ __html: sheet.css }}
+            media={sheet.attrs.media}
+            data-hydrate={sheet.attrs['data-hydrate']}
+            key={i}
+          />
+        ))}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+MyDocument.getInitialProps = async (context: DocumentContext) => {
+  const renderPage = () =>
+    context.renderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) =>
+        (
           <StyletronProvider value={styletron}>
             <App {...props} />
           </StyletronProvider>
         ),
-      });
-
-    const initialProps = await Document.getInitialProps({
-      ...context,
-      renderPage,
     });
-    const stylesheets = (styletron as Server).getStylesheets() || [];
-    return { ...initialProps, stylesheets };
-  }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          {this.props.stylesheets.map((sheet, i) => (
-            <style
-              className="_styletron_hydrate_"
-              dangerouslySetInnerHTML={{ __html: sheet.css }}
-              media={sheet.attrs.media}
-              data-hydrate={sheet.attrs['data-hydrate']}
-              key={i}
-            />
-          ))}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+  const initialProps = await Document.getInitialProps({
+    ...context,
+    renderPage,
+  });
+  const stylesheets = (styletron as Server).getStylesheets() || [];
+  return { ...initialProps, stylesheets };
+};
 
 export default MyDocument;
